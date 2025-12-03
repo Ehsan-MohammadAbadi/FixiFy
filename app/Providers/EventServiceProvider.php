9284117c44f2a8437d46d7311c8233b6aed7fa6b
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-12-03 22:46:58 +0330

    Add TaskerObserver

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Admin;
+use App\Models\Tasker;
 use App\Observers\AdminObserver;
+use App\Observers\TaskerObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -28,6 +30,7 @@ class EventServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Admin::observe(AdminObserver::class);
+        Tasker::observe(TaskerObserver::class);
     }
 
     /**
